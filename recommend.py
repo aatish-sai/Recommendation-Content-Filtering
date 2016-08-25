9c34a34fcda69bb0396184a975d015c79f1b7f59
Aatish Sai <meatish.sai@gmail.com>
2016-08-25 13:35:23 +0545

    creating user with certain preferences

--- recommend.py
+++ recommend.py
@@ -9,4 +9,27 @@ movies_df = pd.concat([movies_df,movies_df.movie_genre.str.get_dummies(sep='|')]
 
 movie_categories = movies_df.columns[3:]
 
-print movies_df.loc[0]
+#print movies_df.loc[0]
+
+from collections import OrderedDict
+
+user_preferences = OrderedDict(zip(movie_categories,[]))
+
+user_preferences['Action'] = 5
+user_preferences['Adventure'] = 5
+user_preferences['Animation'] = 1
+user_preferences["Children's"] = 1
+user_preferences["Comedy"] = 3
+user_preferences['Crime'] = 2
+user_preferences['Documentary'] = 1  
+user_preferences['Drama'] = 1
+user_preferences['Fantasy'] = 5
+user_preferences['Film-Noir'] = 1
+user_preferences['Horror'] = 2
+user_preferences['Musical'] = 1
+user_preferences['Mystery'] = 3
+user_preferences['Romance'] = 1
+user_preferences['Sci-Fi'] = 5
+user_preferences['War'] = 3
+user_preferences['Thriller'] = 2
+user_preferences['Western'] = 1
