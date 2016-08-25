016996bed5a56ad0f585dd2f84e4062d3a20a2f3
Aatish Sai <meatish.sai@gmail.com>
2016-08-25 13:56:07 +0545

    testing values

--- recommend.py
+++ recommend.py
@@ -37,9 +37,9 @@ user_preferences['Western'] = 1
 def get_movie_score(movie_features,user_preferences):
     return np.dot(movie_features,user_preferences)
 
-sample_movie_features = movies_df.loc[0][movie_categories]
+sample_movie_features = movies_df.loc[647][movie_categories]
 
-#print sample_movie_features
+print sample_movie_features
 
 user_predicted_score = np.dot(sample_movie_features,user_preferences.values())
 
