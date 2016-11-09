3613fd848bb40e34ad3dd7efbdce1ebc0e1a7a44
Aatish Sai <meatish.sai@gmail.com>
2016-11-09 21:44:42 +0545

    final

--- recommend.py
+++ recommend.py
@@ -15,21 +15,21 @@ from collections import OrderedDict
 
 user_preferences = OrderedDict(zip(movie_categories,[]))
 
-user_preferences['Action'] = 5
-user_preferences['Adventure'] = 5
+user_preferences['Action'] = 2
+user_preferences['Adventure'] = 3
 user_preferences['Animation'] = 1
 user_preferences["Children's"] = 1
-user_preferences["Comedy"] = 3
-user_preferences['Crime'] = 2
-user_preferences['Documentary'] = 1
-user_preferences['Drama'] = 1
-user_preferences['Fantasy'] = 5
+user_preferences["Comedy"] = 2
+user_preferences['Crime'] = 3
+user_preferences['Documentary'] = 2
+user_preferences['Drama'] = 3
+user_preferences['Fantasy'] = 2
 user_preferences['Film-Noir'] = 1
-user_preferences['Horror'] = 2
+user_preferences['Horror'] = 5
 user_preferences['Musical'] = 1
-user_preferences['Mystery'] = 3
-user_preferences['Romance'] = 1
-user_preferences['Sci-Fi'] = 5
+user_preferences['Mystery'] = 5
+user_preferences['Romance'] = 3
+user_preferences['Sci-Fi'] = 2
 user_preferences['War'] = 3
 user_preferences['Thriller'] = 2
 user_preferences['Western'] = 1
@@ -37,7 +37,7 @@ user_preferences['Western'] = 1
 def get_movie_score(movie_features,user_preferences):
     return np.dot(movie_features,user_preferences)
 
-#sample_movie_features = movies_df.loc[648][movie_categories]
+sample_movie_features = movies_df.loc[648][movie_categories]
 
 #print sample_movie_features
 
@@ -45,11 +45,16 @@ def get_movie_score(movie_features,user_preferences):
 
 #print user_predicted_score
 
-print movies_df[movie_categories].head(1)
-print user_preferences.values()
+#print movies_df[movie_categories].head(1)
+#print user_preferences.values()
 
 def get_movie_recommendation(user_preferences,number):
     movies_df['score'] = movies_df[movie_categories].apply(get_movie_score,args=([user_preferences.values()]),axis=1)
     return movies_df.sort_values(by=['score'],ascending=False)['movie_title'][:number]
 
+def get_genre(user_preferences,number):
+    movies_df['score'] = movies_df[movie_categories].apply(get_movie_score,args=([user_preferences.values()]),axis=1)
+    return movies_df.sort_values(by=['score'],ascending=False)['movie_genre'][:number]
+
 print get_movie_recommendation(user_preferences,10)
+print get_genre(user_preferences,10)
